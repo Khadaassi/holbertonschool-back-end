0af68f649700f97922731568243922c27069a1a4
Khadaassi <aassikhadija@yahoo.fr>
2024-04-15 18:16:13 +0200

    Refactor API script to export user tasks to JSON format and update README.md

---
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" Export data in the JSON format """
+import json
+import requests
+import sys
+
+def export_user_tasks(url):
+    try:
+        users = requests.get(url).json()
+        all_user_tasks = {}
+        for user in users:
+            userId = user.get("id")
+            username = user.get("username")
+            todos = requests.get(url + f"/{userId}/todos").json()
+            user_tasks = [{"username": username,
+                           "task": todo.get("title"),
+                           "completed": todo.get("completed")} for todo in todos]
+            all_user_tasks[userId] = user_tasks
+        with open("todo_all_employees.json", mode='w') as file:
+            json.dump(all_user_tasks, file)
+        print("Data for all users exported to todo_all_employees.json successfully.")
+    except requests.RequestException as e:
+        print(f"Error fetching data: {e}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/users"
+    export_user_tasks(url)
