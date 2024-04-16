4fc01f88c019326ae12f0a2fe0087db848fe5f2a
Khadaassi <aassikhadija@yahoo.fr>
2024-04-16 21:25:01 +0200

    Refactor API script to handle exceptions when retrieving employee tasks and improve error handling

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,32 +1,31 @@
 #!/usr/bin/python3
-""" using this REST API in CSV format"""
+""" using this REST API """
 
-import csv
 import requests
-from sys import argv
+import sys
 
+API_URL = "https://jsonplaceholder.typicode.com/"
 
-API_URL = 'https://jsonplaceholder.typicode.com'
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: ./0-gather_data_from_an_API.py <employee id>")
+        sys.exit(1)
 
-if __name__ == '__main__':
-    USER_ID = argv[1]
+    id = sys.argv[1]
 
-    """ check user's informations """
-    user_response = requests.get(f"{API_URL}/users/{USER_ID}").json()
+    """ check user's information """
+    employee = requests.get(API_URL + "users/{}".format(id)).json()
 
     """ check user's to do list """
-    todo_response = requests.get(f"{API_URL}/todos?userId={USER_ID}").json()
+    todo_list = requests.get("{}todos?userId={}".format(API_URL, id)).json()
 
-    """ write to csv file"""
-    with open(f"{USER_ID}.csv", mode='w') as csv_file:
-        writter = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
+    """ filter for task complete """
+    completed_tasks = [task.get("title")
+                       for task in todo_list if task.get("completed") is True]
 
-        for task in todo_response:
-            writter.writerow([
-                user_response['id'],
-                user_response['username'],
-                task['completed'],
-                task['title']
-            ])
+    """ display progression """
+    print("Employee {} is done with tasks({}/{}):".format(
+        employee.get("name"), len(completed_tasks), len(todo_list)))
 
-        print(f"Data as been exported to {USER_ID}.csv")
+    for task in completed_tasks:
+        print("\t {}".format(task))
